4ce01a073131067064588e06de470162110e5ddc
Samuel Yazie <samuelwassie169@gmail.com>
2023-11-10 19:10:07 -0600

    finished project

---
+++ assets/script/index.js
@@ -0,0 +1,25 @@
+'use strict';
+
+function onEvent(event, selector, callback) {
+    return selector.addEventListener(event, callback);
+}
+
+function selectById(selector, parent = document) {
+    return parent.getElementById(selector);
+}
+
+function select(selector, parent = document) {
+    return parent.querySelector(selector);
+}
+
+function selectAll(selector, parent = document) {
+    return[...parent.querySelectorAll(selector)];
+}
+
+function create(element, parent = document) {
+    return parent.createElement(element);
+}
+
+function print(...args) {
+    console.log(args.join(', '));
+}
