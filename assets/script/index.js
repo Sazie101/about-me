5b01ef3428095041a9ecaed7b1a1fca8dc7dc9ac
Samuel Yazie <samuelwassie169@gmail.com>
2023-11-18 20:16:00 -0600

    updating my portfolio

--- assets/script/index.js
+++ assets/script/index.js
@@ -13,7 +13,7 @@ function select(selector, parent = document) {
 }
 
 function selectAll(selector, parent = document) {
-    return[...parent.querySelectorAll(selector)];
+    return [...parent.querySelectorAll(selector)];
 }
 
 function create(element, parent = document) {
@@ -24,19 +24,52 @@ function print(...args) {
     console.log(args.join(', '));
 }
 
+const menu = selectById('hamburger');
+const navbar = select('.menus');
 const projectPage = select('.projectPage');
-const aboutMe = select('.aboutMe');
-const contactPage = select('.contactPage');
+const aboutMe = select('.about');
+const contactPage = select('.contactMe');
 const buttons = selectAll('.info');
+const scrollToTopBtn = selectById('scrollToTopBtn');
 
 buttons.forEach((button) => {
     onEvent('click', button, () => {
-        if (button.classList.contains('project')) {
+        if (button.classList.contains('myProject')) {
             projectPage.scrollIntoView({ behavior: 'smooth' });
-        } else if (button.classList.contains('about')) {
+        } else if (button.classList.contains('aboutMe')) {
             aboutMe.scrollIntoView({ behavior: 'smooth' });
         } else if (button.classList.contains('contact')) {
             contactPage.scrollIntoView({ behavior: 'smooth' });
         }
+        navbar.style.display = 'none';
     });
 });
+
+onEvent('scroll', window, () => {
+    if (window.pageYOffset > 100) {
+        scrollToTopBtn.style.display = 'block';
+    } else {
+        scrollToTopBtn.style.display = 'none';
+    }
+});
+
+onEvent('click', scrollToTopBtn, () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+});
+
+onEvent('click', menu, () => {
+    navbar.style.display = (navbar.style.display === 'block') ? 'none' : 'block';
+});
+
+function checkWindowWidth() {
+    const windowWidth = window.innerWidth;
+    if (windowWidth >= 911) {
+        navbar.style.display = 'none';
+    }
+}
+
+onEvent('resize', window, () => {
+    checkWindowWidth();
+});
+
+checkWindowWidth();
